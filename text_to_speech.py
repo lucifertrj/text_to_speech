0e84fa561d112efc738f04dcd554a426db5075a8
lucifertrj <66197713+lucifertrj@users.noreply.github.com>
2021-02-08 22:19:54 +0530

    Update and rename text_to_speech to text_to_speech.py

--- text_to_speech.py
+++ text_to_speech.py
@@ -2,18 +2,24 @@ import pyttsx3 as ts
 import time
 
 speak = ts.init()
+
 name=input('Enter name:')
 greet=input('How you feel today?').lower()
 speak.say('Hello')
 time.sleep(2)
+
 speak.say('I am Lucifer')
+
 time.sleep(2)
+
 speak.say(f'I repeat whatever {name} tells')
 time.sleep(2)
+
 if 'good' in greet or 'cool' in greet or 'great' in greet:
 	speak.say('ok, have a nice day')
 else:
 	speak.say('Better luck next time')
+
 speak.runAndWait()
 
 
